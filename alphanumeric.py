f776f7ca98bbfaebcd32c51d8ee0d6ec1d7a065d
MackEdweise <marcusedwards@Hotmail.ca>
2015-12-29 16:17:56 -0500

    Create alphanumeric.py
    Included in the security system was a dot matrix. We wrote a library called pixelprint for easy control of the dot matrix. This file contains the letters and numbers that can be printed to the dot matrix.

---
+++ alphanumeric.py
@@ -0,0 +1,495 @@
+"""
+-------------------------------------------------------
+alphanumeric.py
+alphanumeric pixelation component
+-------------------------------------------------------
+Author:  William Yang & Marcus Edwards
+Version: 2015-12-20
+-------------------------------------------------------
+"""
+# 5x8 image of number 0
+number_0 = \
+  [[0, 1, 2, 3, 4],
+	 [0,          4],
+   [0,          4],
+	 [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0, 1, 2, 3, 4]]
+
+# 5x8 image of number 1
+number_1 = \
+  [[      2,     ],
+	 [      2,     ],
+   [      2,     ],
+	 [      2,     ],
+   [      2,     ],
+   [      2,     ],
+   [      2,     ],
+   [      2,     ]]
+
+# 5x8 image of number 2
+number_2 = \
+	[[      2,     ],
+	 [   1,    3,  ],
+   [            4],
+	 [            4],
+   [         3,  ],
+   [      2      ],
+   [1            ],
+   [0, 1, 2, 3, 4]]
+
+# 5x8 image of number 3
+number_3 = \
+	[[0, 1, 2, 3,  ],
+	 [            4],
+   [            4],
+	 [   1, 2, 3, 4],
+   [            4],
+   [            4],
+   [            4],
+   [0, 1, 2, 3,  ]]
+
+# 5x8 image of number 4
+number_4 = \
+	[[0,          4],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [            4],
+   [            4],
+   [            4],
+   [            4]]
+
+# 5x8 image of number 5
+number_5 = \
+	[[0, 1, 2, 3, 4],
+	 [0            ],
+   [0            ],
+	 [0            ],
+   [0, 1, 2, 3, 4],
+   [            4],
+   [            4],
+   [0, 1, 2, 3, 4],
+	]
+
+# 5x8 image of number 6
+number_6 = \
+	[[0, 1, 2, 3, 4],
+	 [0            ],
+   [0            ],
+	 [0, 1, 2, 3, 4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0, 1, 2, 3, 4],
+	]
+
+# 5x8 image of number 7
+number_7 = \
+	[[0, 1, 2, 3, 4],
+	 [            4],
+   [            4],
+	 [            4],
+   [            4],
+   [            4],
+   [            4],
+   [            4]]
+
+# 5x8 image of number 8
+number_8 = \
+	[[0, 1, 2, 3, 4],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0, 1, 2, 3, 4]]
+
+# 5x8 image of number 9
+number_9 = \
+	[[0, 1, 2, 3, 4],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [            4],
+   [            4],
+   [            4],
+   [            4]]
+
+# 5x8 image of number dot
+number_dot = \
+	[[             ],
+	 [             ],
+	 [             ],
+   [             ],
+   [             ],
+   [             ],
+   [             ],
+   [      2,     ]]
+
+# 5x8 image of letter A
+letter_a = \
+	[[	2,     ],
+	 [   1,    3   ],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4]]
+
+# 5x8 image of letter B
+letter_b = \
+	[[0, 1, 2, 3,  ],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2,  3, 4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0, 1, 2, 3,  ]]
+
+# 5x8 image of letter C
+letter_c = \
+	[[   1, 2, 3, 4],
+	 [0,           ],
+   [0,           ],
+	 [0,           ],
+   [0,           ],
+   [0,           ],
+   [0,           ],
+   [   1, 2, 3, 4]]
+
+# 5x8 image of letter D
+letter_d = \
+	[[0, 1, 2, 3, 4],
+	 [   1,       4],
+   [   1,       4],
+	 [   1,       4],
+   [   1,       4],
+   [   1,       4],
+   [   1,       4],
+   [0, 1, 2, 3, 4]]
+
+# 5x8 image of letter E
+letter_e = \
+	[[   1, 2, 3, 4],
+	 [0,           ],
+   [0,           ],
+	 [0, 1, 2, 3, 4],
+   [0,           ],
+   [0,           ],
+   [0,           ],
+   [   1, 2, 3, 4]]
+
+# 5x8 image of letter F
+letter_f = \
+	[[0, 1, 2, 3, 4],
+	 [0,           ],
+   [0,           ],
+	 [0,           ],
+   [0, 1, 2, 3, 4],
+   [0,           ],
+   [0,           ],
+   [0,	       ]]
+
+# 5x8 image of letter G
+letter_g = \
+	[[   1, 2, 3, 4],
+	 [0,           ],
+   [0,           ],
+	 [0,           ],
+   [0,    2, 3, 4],
+   [0,          4],
+   [0,          4],
+   [   1, 2, 3, 4]]
+
+# 5x8 image of letter H
+letter_h = \
+	[[0,          4],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4]]
+
+# 5x8 image of letter I
+letter_i = \
+	[[   1, 2, 3,  ],
+	 [      2,     ],
+	 [      2,     ],
+	 [      2,     ],
+	 [      2,     ],
+   [      2,     ],
+   [      2,     ],
+	 [   1, 2, 3,  ]]
+
+# 5x8 image of letter J
+letter_j = \
+	[[   1, 2, 3,  ],
+	 [         3,  ],
+	 [         3,  ],
+	 [         3,  ],
+	 [         3,  ],
+   [         3,  ],
+   [   1,    3,  ],
+	 [      2, 3,  ]]
+
+# 5x8 image of letter K
+letter_k = \
+	[[0,          4],
+	 [0,          4],
+   [0,       3,  ],
+	 [0, 1, 2,     ],
+   [0,    2,     ],
+   [0,       3,  ],
+   [0,          4],
+   [0,          4]]
+
+# 5x8 image of letter L
+letter_l = \
+	[[0,           ],
+	 [0,           ],
+   [0,           ],
+	 [0,           ],
+   [0,           ],
+   [0,           ],
+   [0,           ],
+   [0, 1, 2, 3, 4]]
+
+# 5x8 image of letter M
+letter_m = \
+	[[0,          4],
+	 [0, 1,    3, 4],
+   [0,    2,    4],
+	 [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4]]
+
+# 5x8 image of letter N
+letter_n = \
+	[[0,          4],
+	 [0, 1,       4],
+   [0,    2,    4],
+	 [0,       3, 4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4]]
+
+# 5x8 image of letter O
+letter_o = \
+	[[0, 1, 2, 3, 4],
+	 [0,          4],
+   [0,          4],
+	 [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [0, 1, 2, 3, 4]]
+
+# 5x8 image of letter P
+letter_p = \
+	[[0, 1, 2, 3, 4],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [0,           ],
+   [0,           ],
+   [0,           ],
+   [0, 	       ]]
+
+# 5x8 image of letter Q
+letter_q = \
+	[[0, 1, 2, 3,  ],
+	 [0,       3,  ],
+   [0,       3,  ],
+	 [0,       3,  ],
+   [0,       3,  ],
+   [0,    2, 3,  ],
+   [0, 1, 2, 3,  ],
+   [ 	      4]]
+
+# 5x8 image of letter R
+letter_r = \
+	[[0, 1, 2, 3, 4],
+	 [0,          4],
+   [0,          4],
+	 [0, 1, 2, 3, 4],
+   [0, 1,        ],
+   [0,    2,     ],
+   [0,       3,  ],
+   [0, 	      4]]
+
+# 5x8 image of letter S
+letter_s = \
+	[[   1, 2, 3, 4],
+	 [0,           ],
+   [0,           ],
+	 [0, 1, 2, 3, 4],
+   [            4],
+   [            4],
+   [            4],
+   [0, 1, 2, 3,  ]]
+
+# 5x8 image of letter T
+letter_t = \
+	[[0, 1, 2, 3, 4],
+	 [      2,     ],
+   [      2,     ],
+	 [      2,     ],
+   [      2,     ],
+   [      2,     ],
+   [      2,     ],
+   [      2,     ]]
+
+# 5x8 image of letter U
+letter_u = \
+	[[0,          4],
+	 [0,          4],
+   [0,          4],
+	 [0,          4],
+   [0,          4],
+   [0,          4],
+   [0,          4],
+   [   1, 2, 3,  ]]
+
+# 5x8 image of letter V
+letter_v = \
+	[[0,          4],
+	 [0,          4],
+   [0,          4],
+	 [0,          4],
+	 [0,          4],
+   [0,          4],
+   [   1,    3,  ],
+   [      2      ]]
+
+# 5x8 image of letter W
+letter_w = \
+	[[0,          4],
+	 [0,          4],
+   [0,          4],
+	 [0,          4],
+	 [0,          4],
+	 [0,    2,    4],
+   [0,    2,    4],
+   [   1,    3,  ]]
+
+# 5x8 image of letter X
+letter_x = \
+	[[0,          4],
+	 [   1,    3,  ],
+   [   1,    3,  ],
+	 [      2,     ],
+	 [      2,     ],
+	 [   1,    3,  ],
+   [   1,    3,  ],
+   [0,          4]]
+
+
+# 5x8 image of letter Y
+letter_y = \
+	[[0,          4],
+	 [   1,    3,  ],
+	 [      2,     ],
+	 [      2,     ],
+	 [      2,     ],
+	 [      2,     ],
+	 [      2,     ],
+	 [      2,     ]]
+
+
+# 5x8 image of letter Z
+letter_z = \
+	      [[0, 1, 2, 3, 4],
+	       [            4],
+         [         3,  ],
+	       [      2,     ],
+         [      2,     ],
+         [   1,        ],
+         [0,           ],
+         [0, 1, 2, 3, 4]]
+
+
+# 5x8 image of invalid letter
+invalid = \
+	      [[0, 1, 2, 3, 4],
+	       [0, 1, 2, 3, 4],
+         [0, 1, 2, 3, 4],
+      	 [0, 1, 2, 3, 4],
+         [0, 1, 2, 3, 4],
+         [0, 1, 2, 3, 4],
+         [0, 1, 2, 3, 4],
+         [0, 1, 2, 3, 4]]
+
+# Function to find pixel version of alphanumerics
+def pixelize(char):
+
+	if(len(char) > 1):
+		return invalid
+			
+	else:
+		return{
+
+			'a': letter_a, 
+			'b': letter_b, 
+			'c': letter_c, 
+			'd': letter_d, 
+			'e': letter_e, 
+			'f': letter_f, 
+			'g': letter_g, 
+			'h': letter_h, 
+			'i': letter_i, 
+			'j': letter_j, 
+			'k': letter_k, 
+			'l': letter_l, 
+			'm': letter_m, 
+			'n': letter_n, 
+			'o': letter_o, 
+			'p': letter_p, 
+			'q': letter_q, 
+			'r': letter_r, 
+			's': letter_s, 
+			't': letter_t, 
+			'u': letter_u, 
+			'v': letter_v, 
+			'w': letter_w, 
+			'x': letter_x, 
+			'y': letter_y, 
+			'z': letter_z,
+			'0': number_0, 		
+			'1': number_1, 
+			'2': number_2, 
+			'3': number_3, 
+			'4': number_4,
+			'5': number_5, 
+			'6': number_6,
+			'7': number_7, 
+			'8': number_8, 
+			'9': number_9, 
+			'.': number_dot, 
+			',': number_dot, 
+
+		}.get(char, invalid)
+
+# 5x8 image of letters A-Z
+# It's like singing the alphabet song. 
+alphabet = [ \
+	letter_a, letter_b, letter_c, letter_d, letter_e, letter_f, 
+	letter_g, letter_h, letter_i, letter_j, letter_k, letter_l,
+	letter_m, letter_n, letter_o, letter_p, letter_q, letter_r,
+	letter_s, letter_t, letter_u, letter_v, letter_w, letter_x,
+	letter_y, letter_z]
+
+# 5x8 image of numbers 0-9
+numbers = [\
+	number_0, number_1, number_2, number_3, number_4, number_5,
+	number_6, number_7, number_8, number_9]
